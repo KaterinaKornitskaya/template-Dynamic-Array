a5d654fb1cdaff3c409b90c2aecdda8899274aba
Радченко Катерина <ketrinradchenko@gmail.com>
2023-02-02 22:59:15 +0200

    new

--- template Dynamic Array.cpp
+++ template Dynamic Array.cpp
@@ -11,8 +11,8 @@
 
 template <typename T> class DynArray  // шаблонный класс Динамический Массив
 {
-	T* arr;  // указатель на массив
-	T size;  // размер массива
+	T* arr;    // указатель на массив
+	int size;  // размер массива
 public:
 	DynArray() : arr{nullptr}, size{0} {}
 
@@ -31,7 +31,7 @@ public:
 
 	void fill_elements()  // метод - добавление эл-тов в массив
 	{			
-		T x = 0;  // для заполнения массива пользователем
+		T x = 0;                        // для заполнения массива пользователем
 		for (int i = 0; i < size; ++i)  // до тех пор, пока < size
 		{                               // (size передаем в конструкторе)			
 			std::cout << "Put element: ";
@@ -40,36 +40,32 @@ public:
 		}				
 	}
 
-	void add_new(int elem)  // метод - добавление еще одного эл-та
+	void add_new(T elem, int &size)  // метод - добавление еще одного эл-та
 	{
-		T* temp = new T[size + 1];  // выделить новую память на размер size + 1
+		T* temp = new T[size + 1];   // выделить новую память на размер size + 1
 		for (int i = 0; i < size; i++)
 		{
-			temp[i] = arr[i];  // присвоили "новому" массивы "старый"
+			temp[i] = arr[i];        // присвоили новым эл-там старые
 		}
-		temp[size] = elem;
-		delete[] arr;  // удаление старой памяти
+		temp[size] = elem;           // в последний эл-т - новый добавленный вписали значение
+		delete[] arr;                // удаление старой памяти
 		arr = temp;
-		for (int i = 0; i < size+1; i++)
-		{
-			std::cout << arr[i] << ' ';  // вывод нового массива
-		}
-		size++;
+		size++;                      // увеличиваем размер массива
 	}
 
-	void delete_elem(int index)  // // метод - удаление одного эл-та
+	void delete_elem(int index, int& size)  // метод - удаление определенного эл-та
 	{		
 		for (int i = 0; i < size; i++)
 		{			
-			if (i != index)
-			{				
-				std::cout << arr[i] << ' ';
-			}			
+			if (i >= index)                 // если эл-т >= индексу, то приравниваем
+			{                               // текущий эл-т к следующему
+				arr[i] = arr[i + 1];
+			}                               // если эл-т < индекса - оставляем его, как есть		
 		}
-		size -= 1;
+		size --;                            // уменьшаем размер массива
 	}
 
-	void print_element()  // метод - вывод на экран
+	void print_element(int &size)  // метод - вывод на экран
 	{
 		for (int i{ 0 }; i < size ; ++i)
 		{
@@ -84,46 +80,49 @@ public:
 
 	~DynArray()
 	{
-		delete[] arr;
+		delete[] arr;  // освобождаем память
 	}
 };
 
 int main()
 {	
 	int size = 3;
-	DynArray<int> arr1(size++);  // создаем конкретный объект int
+	DynArray<int> arr1(size);  // создаем конкретный объект int
 		
-	arr1.fill_elements();  // заполнение массива
-	arr1.print_element();  // вывод на экран
+	arr1.fill_elements();      // заполнение массива
+	arr1.print_element(size);  // вывод на экран
 	std::cout << std::endl;
 	
+	// дальше повтор ф-ий для видимости результатов
 	std::cout << "\nAdd element: \n";
-	arr1.add_new(2);  // добавление эл-та
-	std::cout << "\nDelete element: \n";
-	arr1.delete_elem(0);  // удаление эл-та
-
-	/*
-		Здесь у меня проблема. Если я хочу прописала ф-ию добавления и 
-		удаления по одному разу - все ок.
-		А если я хочу добавить несколько раз подряд - просто каждый раз
-		добавляет другой элемент.
-		Я не пойму, как сдела так, что бы после 3 введенный можно
-		было дальше добалять еще много.
-	*/
-	
+	arr1.add_new(7, size);      // добавление эл-та
+	arr1.print_element(size);   // вывод на экран
 	std::cout << std::endl;
-	DynArray<char> arr2(size++);  // создаем конкретный объект char
+	arr1.add_new(8, size);      // добавление эл-та
+	arr1.print_element(size);   // вывод на экран
+	std::cout << std::endl;
+	arr1.add_new(9, size);      // добавление эл-та
+	arr1.print_element(size);   // вывод на экран
+	std::cout << "\nDelete element: \n";
+	arr1.delete_elem(0, size);  // удаление эл-та
+	arr1.print_element(size);   // вывод на экран
+	std::cout << std::endl;
+	arr1.delete_elem(0, size);  // удаление эл-та
+	arr1.print_element(size);   // вывод на экран
 
+	// аналогично для объекта char:
+	DynArray<char> arr2(size);  // создаем конкретный объект char
+	std::cout << std::endl;
 	arr2.fill_elements();
-	arr2.print_element();
+	arr2.print_element(size);
 	std::cout << std::endl;
 	std::cout << "\nAdd element: \n";
-	arr2.add_new(4);
+	arr2.add_new('a', size);
+	arr2.print_element(size);   
 	std::cout << "\nDelete element: \n";
-	arr2.delete_elem(1);
+	arr2.delete_elem(1, size);
+	arr2.print_element(size);   
 
 	return 0;
 }
-
-
 
